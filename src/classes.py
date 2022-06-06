8f2ce8d3acd8bca7f82e0e74ea863c0c9ba60233
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-06 23:45:03 +0300

    Added new class.

--- src/classes.py
+++ src/classes.py
@@ -2,8 +2,36 @@ import time
 import dataclasses
 import logging
 import curses
+import enum
+import typing
 
 logger = logging.getLogger(__name__)
+
+
+class IterationResult(enum.Enum):
+    fail = 'FAIL'
+    ok = 'OK'
+    skip = 'SKIP'
+
+
+typing.Literal[IterationResult.fail, IterationResult.ok, IterationResult.skip]
+
+
+class Iteration():
+    char: str
+    result: str | None
+    time_start: float | None
+    time_end: float | None
+
+    def __init__(self, char):
+        char = str(char)
+        if len(char) != 1:
+            raise ValueError
+
+        self.char = char
+        self.result = None
+        self.time_start = None
+        self.time_end = None
 
 
 @dataclasses.dataclass
